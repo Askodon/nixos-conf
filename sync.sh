8366a006ecfafdf04b95ea3acef4f33e96e8ae84
Матвей Баканов <103024741+Askodon@users.noreply.github.com>
2024-07-18 15:28:39 +0400

    test

--- sync.sh
+++ sync.sh
@@ -4,4 +4,3 @@ cp /etc/nixos/configuration.nix $WORK_DIR
 cp -r /home/askodon/.config/rofi $WORK_DIR
 cp /home/askodon/.config/home-manager/home.nix $WORK_DIR
 cp /home/askodon/.config/home-manager/all.nix $WORK_DIR
-#dconf dump /org/gnome/ | dconf2nix > gnome.nix #test
