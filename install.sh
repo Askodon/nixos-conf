85dd902ffdb24c01f209f4652d04d7d10a468052
Матвей Баканов <103024741+Askodon@users.noreply.github.com>
2024-07-24 11:00:28 +0400

    git qogir change and switch to nautilus

--- install.sh
+++ install.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 sudo cp -r /home/askodon/nixos-conf/nixos/. /etc/nixos/.
 sudo nixos-rebuild switch
+git clone https://github.com/vinceliuice/Qogir-theme.git /home/askodon/Загрузки/qogir
+/home/askodon/Загрузки/qogir/install.sh -l -c dark
 dconf load / < all.dconf
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+sudo flatpak override --filesystem=xdg-config/gtk-3.0
+sudo flatpak override --filesystem=xdg-config/gtk-4.0
 cp -r rofi /home/askodon/.config
 sudo rm -r /home/askodon/.config/home-manager
 sudo cp /home/askodon/nixos-conf/home-manager/keyboard.nix /home/askodon/.config/home-manager
@@ -10,3 +14,4 @@ sudo cp /home/askodon/nixos-conf/home-manager/home.nix /home/askodon/.config/hom
 sudo cp /home/askodon/nixos-conf/home-manager/all.nix /home/askodon/.config/home-manager
 home-manager switch
 mkdir /home/askodon/wallpaper
+
