aea4db3656c05c9ec93d55ff96b6e81b4b111367
Матвей Баканов <103024741+Askodon@users.noreply.github.com>
2024-08-22 16:16:36 +0400

    change path to hardware delete stange foledr

--- scripts/upgrade.sh
+++ scripts/upgrade.sh
@@ -4,7 +4,7 @@ gum style \
         --align center --width 35 --margin "1 2" --padding "2 4" \
         "update"
 
-sudo cp /etc/nixos/hardware-configuration.nix /home/askodon/nixos-conf/nixos/hardware-configuration.nix
+sudo cp /etc/nixos/hardware-configuration.nix /home/askodon/nixos-conf/nixos/system-conf/hardware-configuration.nix
 sudo nixos-rebuild switch --flake /home/askodon/nixos-conf/nixos
 
 gum spin --spinner meter --title "finish update" -- sleep 1
