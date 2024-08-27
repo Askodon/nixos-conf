5a06b8b416b3a51aaf15b3a2f1191641c7048162
Матвей Баканов <103024741+Askodon@users.noreply.github.com>
2024-08-27 16:55:45 +0400

    change script to doas

--- scripts/upgrade.sh
+++ scripts/upgrade.sh
@@ -4,8 +4,8 @@ gum style \
         --align center --width 35 --margin "1 2" --padding "2 4" \
         "update"
 
-sudo cp /etc/nixos/hardware-configuration.nix /home/askodon/nixos-conf/nixos/system-conf/hardware-configuration.nix
-sudo nixos-rebuild switch --flake /home/askodon/nixos-conf/nixos
+doas cp /etc/nixos/hardware-configuration.nix /home/askodon/nixos-conf/nixos/system-conf/hardware-configuration.nix
+doas nixos-rebuild switch --flake /home/askodon/nixos-conf/nixos
 
 gum spin --spinner meter --title "finish update" -- sleep 1
 exec zsh
