d9baa3154ecc03d654f0314691ee9237f10820ec
Матвей Баканов <103024741+Askodon@users.noreply.github.com>
2024-08-28 09:42:00 +0400

    change path to configuration.nix

--- scripts/new_install.sh
+++ scripts/new_install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 nix-shell -p git
 cd /etc/nixos
-echo "nix.settings.experimental-features = [ "nix-command" "flakes" ];" >> conf.nix
+echo "nix.settings.experimental-features = [ "nix-command" "flakes" ];" >> /etc/nixos/configuration.nix
 nixos-rebuild switch
 sudo cp /etc/nixos/hardware-configuration.nix /home/askodon/nixos-conf/nixos/system-conf/hardware-configuration.nix
 sudo nixos-rebuild switch --flake /home/askodon/nixos-conf/nixos
