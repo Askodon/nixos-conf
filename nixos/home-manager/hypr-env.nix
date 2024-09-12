13f728b1528c9a75c7a0220b867f40499f47f6ec
Матвей Баканов <103024741+Askodon@users.noreply.github.com>
2024-09-12 14:21:14 +0400

    test on home

--- nixos/home-manager/hypr-env.nix
+++ nixos/home-manager/hypr-env.nix
@@ -3,18 +3,17 @@
 {
   home = {
     sessionVariables = {
-    EDITOR = "hx";
-    BROWSER = "firefox";
-    TERMINAL = "wezterm";
-    __GL_VRR_ALLOWED="1";
-    WLR_NO_HARDWARE_CURSORS = "1";
-    WLR_RENDERER_ALLOW_SOFTWARE = "1";
-    CLUTTER_BACKEND = "wayland";
-    WLR_RENDERER = "vulkan";
-    NIXOS_OZONE_WL = "1";
-    XDG_CURRENT_DESKTOP = "Hyprland";
-    XDG_SESSION_DESKTOP = "Hyprland";
-    XDG_SESSION_TYPE = "wayland";
+      EDITOR = "hx";
+      BROWSER = "firefox";
+      TERMINAL = "wezterm";
+      __GL_VRR_ALLOWED = "1";
+      WLR_NO_HARDWARE_CURSORS = "1";
+      WLR_RENDERER = "vulkan";
+      CLUTTER_BACKEND = "wayland";
+      WLR_RENDERER_ALLOW_SOFTWARE = "1";
+      XDG_CURRENT_DESKTOP = "Hyprland";
+      XDG_SESSION_DESKTOP = "Hyprland";
+      XDG_SESSION_TYPE = "wayland";
     };
   };
 }
