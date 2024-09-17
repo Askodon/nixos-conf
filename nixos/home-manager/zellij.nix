5bff7edadd6a2b4dc92950fd88f08828e091e1f5
Матвей Баканов <103024741+Askodon@users.noreply.github.com>
2024-09-17 10:22:45 +0400

    qogir colours on zellij and zsh

--- nixos/home-manager/zellij.nix
+++ nixos/home-manager/zellij.nix
@@ -1,20 +1,18 @@
 {
   programs.zellij.enable = true;
-  programs.zellij.settings = ''
-  themes {
-    qogir {
-        fg "#ffffff"
-        bg "#282c34"
-        black "#282c34"
-        red "#e06c75"
-        green "#98c379"
-        yellow "#e5c07b"
-        blue "#4d84c4"
-        magenta "#be5046"
-        cyan "#56b6c2"
-        white "#32343d"
-        orange "#D08770"
-    }
-}
-  '';
+  programs.zellij.settings = {
+  themes.qogir = {
+        fg = "#5CA3F6";
+        bg = "#282c34";
+        black = "#282c34";
+        red = "#e06c75";
+        green = "#5CA3F6";
+        yellow = "#e5c07b";
+        blue = "#4d84c4";
+        magenta = "#be5046";
+        cyan = "#56b6c2";
+        white = "#ffffff";
+        orange = "#14CFA1"; #tip text colour
+    };
+};
 }
