c3c770f7e8e661294bb0a9cc3e03b1c2b9645298
asko <askodon@disroot.org>
2024-09-20 13:54:56 +0400

    full switch to dunst

--- home/programs/dunst/default.nix
+++ home/programs/dunst/default.nix
@@ -8,12 +8,27 @@
     origin = "top-right";
     transparency = 10;
     frame_color = "#eceff1";
-    font = "Droid Sans 9";
+    font = "Roboto 9";
+  };
+
+  urgency_low = {
+    background = "#282c34";
+    foreground = "#66cdaa"; #green
+    frame_color = "#4d84c4";
+    timeout = 10;
   };
 
   urgency_normal = {
-    background = "#37474f";
-    foreground = "#eceff1";
+    background = "#282c34";
+    foreground = "#eceff1"; #white
+    frame_color = "#4d84c4";
+    timeout = 10;
+  };
+
+  urgency_critical = {
+    background = "#282c34";
+    foreground = "#d4182a"; #red
+    frame_color = "#4d84c4";
     timeout = 10;
   };
 };
