ecd912dfd9a9f463f788ee38b3b2024373b68d6d
asko <askodon@disroot.org>
2024-09-24 10:38:43 +0400

    nvchad test

--- flake.nix
+++ flake.nix
@@ -4,10 +4,13 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
     
-
     home-manager = {
       url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
+
+    nvchad4nix = {
+    url = "github:MOIS3Y/nvchad4nix";
+    inputs.nixpkgs.follows = "nixpkgs";
     };
 
   };
