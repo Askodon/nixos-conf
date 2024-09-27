f7ca6d4005bb7a4546040866a1a959300533f8e5
asko <askodon@disroot.org>
2024-09-27 16:55:44 +0400

    add: screenshot script

--- home/scripts/hyprshot.sh
+++ home/scripts/hyprshot.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-
+#TYPE="$(printf "1 - Only clipboard\n2 - Open File\n3 - Normal" | fuzzel -w 40 -y 40 -f "Roboto"-12 --line-height=20 --dmenu -l 7 -p "Clipboard and other: ")"
 SELECTION="$(printf "1 - Fullscreen\n2 - Window\n3 - Region" | fuzzel -w 40 -y 40 -f "Roboto"-12 --line-height=20 --dmenu -l 7 -p "Screenshot type: ")"
 
 case $SELECTION in
@@ -10,3 +10,8 @@ case $SELECTION in
 	*"Region")
 		hyprshot -m region --clipboard-only;;
 esac
+
+
+#hyprshot -o /home/askodon/Скриншоты -m region -- eom
+
+
