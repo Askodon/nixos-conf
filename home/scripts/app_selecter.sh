4758f689a53c913d18eb201d173212ffd8fa681c
asko <askodon@disroot.org>
2024-09-30 12:00:38 +0400

    add: working clipboard manager, add main menu scripts

--- home/scripts/app_selecter.sh
+++ home/scripts/app_selecter.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-fuzzel -T foot --icon-theme WhiteSur-dark -w 40 -y 40 -f "Roboto"-12 --line-height=20 -t ffffffff 
+fuzzel -T foot --icon-theme WhiteSur-dark -w 40 -y 40 -f "Roboto"-12 --line-height=20 -t ffffffff -p "App list:"
---
+++ home/scripts/clipboard.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+clipman pick --tool=CUSTOM --tool-args="fuzzel -d foot --icon-theme WhiteSur-dark -w 40 -y 40 -f "Roboto"-12 --line-height=20 -t ffffffff -p "History:""
