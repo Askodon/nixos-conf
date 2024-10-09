5f929640ec52009d72d5a34fb9fd9cb531542304
Askodon <askodon@disroot.org>
2024-10-09 15:56:10 +0400

    add: timer to auto change wallpaper, option to remove /boot in new_install.sh remove: default hypr wallpaper, option to change wallpaper in main_menu.sh

--- home/scripts/swww.sh
+++ home/scripts/swww.sh
@@ -1,4 +1,7 @@
-#!/usr/bin/env bash
+#!/bin/sh
+
+while true 
+do
 
 DIR=~/wallpaper
 
@@ -14,3 +17,6 @@ swww query || swww init
 echo Setting ${DIR}/${RANDOMPICS}
 
 swww img ${DIR}/${RANDOMPICS} --transition-fps 30 --transition-type any --transition-duration 3
+
+sleep 900
+done
