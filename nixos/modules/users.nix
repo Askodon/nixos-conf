4727eeafe4c3092ad55c36d600360a71d1f56196
Askodon <askodon@disroot.org>
2024-10-14 11:19:26 +0400

    add: artemis folder as main ps add: ares for test playground

--- nixos/modules/users.nix
+++ nixos/modules/users.nix
@@ -10,15 +10,4 @@
       "libvirtd"
     ];
   };
-  #user fot testing
-  users.users.tsumoron = {
-  isNormalUser = true;
-  description = "askodon";
-  extraGroups = [
-    "networkmanager"
-    "wheel"
-    "gamemode"
-    "libvirtd"
-    ];
-  };
 }
