6bcbd76f33d250ca9a46ad057483e7d63cbfe4be
Askodon <askodon@disroot.org>
2024-10-16 08:42:55 +0400

    add: lua module system to lua from guid, nix files

---
+++ home/ares/programs/awesomWM/lua/binding/binding.nix
@@ -0,0 +1,9 @@
+{
+    home.file = {
+    ".config/awesome/binding/bindtotags.rc".source = "./bindtotags.lua";
+    ".config/awesome/binding/clientbuttons.rc".source = "./clientbuttons.lua";
+    ".config/awesome/binding/clientkeys.rc".source = "./clientkeys.lua";
+    ".config/awesome/binding/globalbuttons.rc".source = "./globalbuttons.lua";
+    ".config/awesome/binding/globalkeys.rc".source = "./globalkeys.lua";
+  };
+}
