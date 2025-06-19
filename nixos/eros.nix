4436cbbc462b0cb6f617f2d15d65ba3713a14d02
Askodon <askodon@disroot.org>
2025-06-19 13:06:44 +0400

    add: macos home and fmt

---
+++ nixos/eros.nix
@@ -0,0 +1,24 @@
+{ pkgs, ... }:
+
+{
+    environment.systemPackages = with pkgs; [
+    zellij
+    gum
+    just
+    nh
+];
+
+    nix = {
+        settings.experimental-features = "nix-command flakes";
+        enable = false;
+    };
+    
+    programs.zsh.enable = true;
+    system.stateVersion = 6;
+    nixpkgs.hostPlatform = "aarch64-darwin";
+ 
+    users.users.askodon = {
+        name = "askodon";
+        home = "/Users/askodon";
+    };
+}
